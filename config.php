f9efb80f5e3ff02bf68236ae3cafa7e535c1f2ed
jdpc91 <jdpc91@gmail.com>
2018-03-23 01:41:10 -0600

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $Nombre = ''; //Nomdre del Emisor
 $TipoID = ''; //Tipo de identificacion."01":Fisica,"02":Juridica,"03":DIMEX,"04":NITE
 $NumeroID = ''; //Numero de identificacion
 $NombreComercial = ''; //Nombre de fatacia registrado en hacienda
-//ver codificacion en https://tribunet.hacienda.go.cr/docs/esquemas/2016/v4.2/Codificacionubicacion_V4.2.pdf
+//ver codificacion de la ubicacion en https://tribunet.hacienda.go.cr/docs/esquemas/2016/v4.2/Codificacionubicacion_V4.2.pdf
 $Provincia = ''; 
 $Canton = ''; 
 $Distrito = ''; 
@@ -15,7 +15,9 @@ $CorreoElectronico = ''; //debe ser el mismo registrado en hacienda
 $CodigoMoneda = ''; ////ver tabla de codigos en https://tribunet.hacienda.go.cr/docs/esquemas/2016/v4.2/Codigodemoneda_V4.2.pdf Colon:"CRC" Dolar:"USD"
 $TipoCambio = ''; //Tipo de cambio de la moneda indicada
 $p12Url = ''; //ubicacion del certificado .p12
-$pinP12 = ; //pin de del certificado, int de cuatro digitos
+$pinP12 = ''; //pin de del certificado, int de cuatro digitos
+$inPath = ''; //ubicacion del directorio en donde se crearan los xml sin firmar. Ejemplo "/tmp/"
+$outPath = '';  //ubicacion del directorio en donde se crearan los xml firmados. Ejemplo "/tmp/"
 $username = ''; //nombre de usuario para obtener el token
 $password = ''; //password para obtener el token
  ?>
